8d32cc57835d735b6946e6bff934f2129daf5406
weare22c <160261450+weare22c@users.noreply.github.com>
2024-03-04 20:24:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def main():
                           
                 ╚═══════════════════════════════════════════════╝
                 '''))
-                os.system(f'node TLSVIP {host} {time} 64 1000 proxy.txt')
+                os.system(f'TLSVIP {host} {time} 64 1000 proxy.txt')
             except IndexError:
                 print(Colorate.Diagonal(Colors.yellow_to_red, 'Usage: METHOD URL PORT TIME'));
                 print(Colorate.Diagonal(Colors.yellow_to_red, 'Example: METHOD URL PORT TIME'));
